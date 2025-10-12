260dae10817afc8355640e81b0df43d08d78a4a7
TechShri027 <shrishtibansal2705@gmail.com>
2025-10-12 17:14:56 +0530

    header section created/server created

---
+++ client/src/App.jsx
@@ -0,0 +1,47 @@
+
+import React from 'react'
+import {BrowserRouter, Routes,Route} from 'react-router-dom'
+import Header from './components/Header'
+import Home from './pages/Home'
+import About from './pages/About'
+import Dashboard from './pages/Dashboard'
+import Signup from './pages/Signup'
+import Signin from './pages/Signin'
+import Project from './pages/Project'
+import { Flowbite } from 'flowbite-react';
+
+const customTheme = {
+  navbar: {
+    base: 'bg-white text-black border-b-2', // 👈 yahan apna default color de
+  },
+  button: {
+    color: {
+      primary: 'bg-indigo-600 text-white hover:bg-indigo-700',
+    },
+  },
+  // aur bhi components ka theme yahan set kar sakti hai
+};
+function App() {
+  
+  return (
+    <div>
+  <Flowbite theme={{ theme: customTheme }}></Flowbite>
+   <BrowserRouter>
+   <Header/>
+   <Routes>
+    <Route path='/' element={<Home/>}/>
+    <Route path='about' element={<About/>}/>
+    <Route path='dashboard' element={<Dashboard/>}/>
+    <Route path='signup' element={<Signup/>}/>
+    <Route path='signin' element={<Signin/>}/>
+    <Route path='project' element={<Project/>}/>
+   </Routes>
+   </BrowserRouter>
+    </div>
+   
+  );
+}
+
+  
+
+export default App
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import {Button, Navbar, NavbarCollapse, NavbarLink, NavbarToggle, TextInput} from 'flowbite-react'
+import { Link, useLocation } from 'react-router-dom'
+import { BsSearchHeart } from "react-icons/bs";
+import { FaMoon } from "react-icons/fa";
+
+const Header = () => {
+    const location=useLocation()
+    const path=location.pathname;
+  return (
+    <Navbar className='border-b-2  text-white h-20 pt-6'>
+<Link to='/' className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
+<span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500  rounded-lg text-white'>Web's Tech</span>
+Blogs
+</Link>
+<form>
+  <TextInput
+  type='text'
+  placeholder='Search...'
+  rightIcon={BsSearchHeart}
+  className='hidden lg:inline'
+  />
+</form>
+<Button className='bg-white  lg:hidden' color='gray' >
+    <BsSearchHeart/>
+</Button>
+
+<div className='flex gap-2 md:order-2'>
+    <Button className=' hidden sm:inline' color='gray' pill >
+    <FaMoon/>
+</Button>
+<Link to='/signin'>
+<Button gradientDuoTone='purpleToBlue' outline>
+    SignIn
+</Button>
+</Link>
+<NavbarToggle/>
+</div>
+<NavbarCollapse>
+    <NavbarLink active={path==='/'} as={'div'}>
+        <Link to='/'>Home</Link>
+    </NavbarLink>
+     <NavbarLink active={path==='/about'} as={'div'}>
+        <Link to='/about'>About</Link>
+    </NavbarLink>
+     <NavbarLink active={path==='/project'} as={'div'}>
+        <Link to='/project'>Projects</Link>
+    </NavbarLink>
+
+</NavbarCollapse>
+    </Navbar>
+  )
+}
+
+export default Header

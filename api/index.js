e8c5eb04aba80d52dc083e6f381aaf08c6e35076
TechShri027 <shrishtibansal2705@gmail.com>
2025-10-12 20:08:26 +0530

    create user model and env

--- api/index.js
+++ api/index.js
@@ -1,6 +1,17 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
 
+dotenv.config()
 const app=express()
+
+mongoose.connect(process.env.MONGO_URL)
+.then(()=>{
+    console.log("mongodb is connected")
+}).catch((error)=>{
+    console.log("failed to connected mongodb:",error)
+})
+
 
 app.listen(3000,()=>{
     console.log('app is running on 3000')
